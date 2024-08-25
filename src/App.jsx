e85321931fc3afc6930c692fb7d7e86b6371a350
Sumit.Gupta <Sumit.Gupta@teknopoint.local>
2024-08-25 15:53:01 +0530

    react navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import Product from './pages/Product.jsx'
+import Contact from './pages/Contact.jsx'
+import About from './pages/About.jsx'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/product' element={<Product/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
     </>
   )
 }
---
+++ src/pages/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+const Product = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Product</h1>
+
+    </div>
+  )
+}
+
+export default Product
