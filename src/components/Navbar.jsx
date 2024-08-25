94b0a87f1e8bf59084c0d040bd82a4ec2368c52e
Sumit.Gupta <Sumit.Gupta@teknopoint.local>
2024-08-25 15:16:17 +0530

    navbar links show and hide done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
+import '../css/Navbardesign.css'
 
+import { useEffect, useState } from 'react'
 const Navbar = () => {
+    const [menuVisible, setMenuVisible] = useState(false);
+    // const menuRef = useEffect(null)
+
+    const handleMobileBtn = (e) => {
+        // console.log(e.target.parentElement.parentElement.parentElement.parentElement.querySelector('#mobile-menu').classList.remove('dsp-none'))
+        setMenuVisible(!menuVisible)
+    }
+
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
+        <nav className="navbar">
+            <div className="container">
+                <a href="#" className="brand">Ecommerce</a>
+                <ul className="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">About Us</a></li>
+                </ul>
+                <div className="mobile-menu-btn">
+                    <button id="menu-btn" onClick={(e) => { handleMobileBtn(e) }}>
+                        <svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div id="mobile-menu" className="mobile-nav-links"
+                style={{ display: menuVisible ? "block" : "none" }}
+            >
+                <a href="#">Home</a>
+                <a href="#">Product</a>
+                <a href="#">Contact Us</a>
+                <a href="#">About Us</a>
+            </div>
+        </nav>
+
+
     )
 }
 
